7ce22c24bfacca51bd53264933c031f7214af518
Villfuk02 <vildagut@seznam.cz>
2021-09-06 08:41:04 +0200

    Pollution

--- data/entities/adjustments.lua
+++ data/entities/adjustments.lua
@@ -1,7 +1,10 @@
 -- STONE FURNACE
 data.raw.furnace["stone-furnace"].energy_usage = SODA.constants.processing.power_per_processing_line[1] .. "kW"
 data.raw.furnace["stone-furnace"].crafting_speed = SODA.constants.processing.machine_speeds[1]
+data.raw.furnace["stone-furnace"].energy_source.emissions_per_minute = SODA.constants.processing.pollution_per_processing_line_per_minute[1]
 
 -- BURNER MINER
 data.raw["mining-drill"]["burner-mining-drill"].energy_usage = SODA.constants.processing.power_per_miner[1] .. "kW"
 data.raw["mining-drill"]["burner-mining-drill"].mining_speed = SODA.constants.processing.mining_speeds[1]
+data.raw["mining-drill"]["burner-mining-drill"].energy_source.emissions_per_minute = SODA.constants.processing.pollution_per_miner_per_minute[1]
+
--- data/soda/constants.lua
+++ data/soda/constants.lua
@@ -16,22 +16,30 @@ SODA.constants.processing.machine_speeds = {1, 1, 2, 2}
 SODA.constants.processing.mining_speeds = {0.125, 0.375, 1.25}
 
 SODA.constants.processing.energy_per_ore = {600, 360, 450} -- in kJ, vanilla = 600, 180 (x2)
-SODA.constants.processing.energy_per_ingot = {1152, 900, 1500, 1200} -- in kJ, vanilla = 288, 144, 288 (x8)
+SODA.constants.processing.energy_per_ingot = {1152, 900, 1500, 1200} -- in kJ, vanilla (plates) = 288, 144, 288 (x8)
+
+SODA.constants.processing.pollution_per_60_ore = {96, 36, 25} -- vanilla = 48, 20 (x2)
+SODA.constants.processing.pollution_per_60_ingots = {60, 90, 45, 24} -- vanilla (plates) = 6.4, 6.4, 1.6 (x8)
 
 -- CALCULATED
 SODA.constants.processing.power_per_miner = {} -- in kW
 SODA.constants.processing.power_per_processing_line = {} -- in kW
+SODA.constants.processing.pollution_per_miner_per_minute = {}
+SODA.constants.processing.pollution_per_processing_line_per_minute = {}
 
 SODA.constants.processing.total_ore_per_ingot = {}
 SODA.constants.processing.total_time_per_ingot = {}
+
+for i = 1, SODA.constants.processing.mining_levels, 1 do
+    SODA.constants.processing.power_per_miner[i] = SODA.constants.processing.energy_per_ore[i] * SODA.constants.processing.mining_speeds[i]
+    SODA.constants.processing.pollution_per_miner_per_minute[i] = SODA.constants.processing.pollution_per_60_ore[i] * SODA.constants.processing.mining_speeds[i]
+end
 
 for i = 1, SODA.constants.processing.processing_levels, 1 do
     SODA.constants.processing.total_ore_per_ingot[i] = SODA.constants.processing.ore_per_ingot / SODA.constants.processing.productivity[i]
     SODA.constants.processing.total_time_per_ingot[i] = SODA.constants.processing.total_ore_per_ingot[i] * SODA.constants.processing.lines_per_basic_belt[i] *
                                                             SODA.constants.processing.machine_speeds[i] / SODA.constants.belt_speeds[1]
     SODA.constants.processing.power_per_processing_line[i] = SODA.constants.processing.energy_per_ingot[i] / SODA.constants.processing.total_time_per_ingot[i]
+    SODA.constants.processing.pollution_per_processing_line_per_minute[i] = SODA.constants.processing.pollution_per_60_ingots[i] / SODA.constants.processing.total_time_per_ingot[i]
 end
 
-for i = 1, SODA.constants.processing.mining_levels, 1 do
-    SODA.constants.processing.power_per_miner[i] = SODA.constants.processing.energy_per_ore[i] * SODA.constants.processing.mining_speeds[i]
-end
